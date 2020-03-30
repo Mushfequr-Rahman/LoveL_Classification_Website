b53b431689ddc700cf31ea92ae3b33e4d0f65eaf
meifish <meifish.kat@gmail.com>
2020-03-30 16:18:58 -0400

    1/ Bridge and setup Vue.js Single Point Application framework  2/ Setup npm serve entry point 3/ Configure relative path alias to be used in Vue component.

---
+++ src/front-end/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header :title="data" />
+    <Nav/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Nav
+  },
+  data () {
+    return {
+      data: 'Kathryn'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/front-end/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/contacts">Contacts</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
